a274e3af9c73a38287bbaae14d7e398541444865
zhu xia <597204139@qq.com>
2017-06-17 23:28:54 +0800

    spring mvc

--- src/main/Java/com/dagou/Hello.java
+++ src/main/Java/com/dagou/Hello.java
@@ -1,16 +1,24 @@
 package com.dagou;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by bobo on 2017/6/17.
  */
-@RestController
-@RequestMapping("/a")
+@Controller
 public class Hello {
-    @RequestMapping("/b")
-    public String he(){
-        return "Hello,dagou";
+    @RequestMapping("/")
+    public String HomePage(@RequestParam(value="name",required = false,defaultValue = "Dagou") String name, Model model){
+        model.addAttribute("name",name);
+        return "HomePage";
+    }
+    @RequestMapping("/u")
+    public String hello(@RequestParam(value="name", required=false, defaultValue="Dagou") String name, Model model){
+        model.addAttribute("name",name);
+        return "greeting";
     }
 }
