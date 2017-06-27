dd85cab3cb0bfc7de6194936952c93cb1aad2659
banboozh <zhuxia_forver@163.com>
2017-06-27 18:40:08 +0800

    hanoi

---
+++ src/Test/Java/com/dagou/HanoiTest.java
@@ -0,0 +1,22 @@
+package com.dagou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by xzhu on 6/27/2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HanoiTest {
+    @Autowired
+    private Hanoi hanoi;
+
+    @Test
+    public void test_Hanoi() throws Exception {
+        hanoi.getNum(3);
+    }
+}
